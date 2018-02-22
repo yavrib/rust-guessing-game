29246710b7a5b8886e49c9a19e929c1f8739a50b
Berkan <Yavrib@gmail.com>
2018-02-23 00:07:04 +0300

    Fixed a cool error.
    I forgot to remove `expect` when I changed `guess.trim().parse().expect()` into `match guess.trim().parse() { /* magic */ }`. It looked like this for a while `match guess.trim().parse() { /* magic */ }.expect()`. Silly me.

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,7 @@ fn main() {
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue
-        }
-            .expect("Please enter a number!");
+        };
 
         println!("Your guess is: {}", guess);
 
